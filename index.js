31cda5f5a9e84ad7c725239865d3d57829d67186
Logiz <l.samemami@gmail.com>
2016-10-09 01:47:48 +0200

    large

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Eris = require('eris');
 const config = require('./config.js');
 
-var redis = require('redis'),
-    client = redis.createClient(null, null);
-
 var Trello = require("node-trello");
 var t = new Trello(config.trelloKey, config.trelloToken);
 
@@ -19,15 +16,12 @@ bot.on("error", err => {
 bot.on("ready", () => {
     console.log('Ready!');
 });
-var canarySKey;
-var iOSSKey;
-var androidSKey;
 
 bot.on('messageCreate', (msg) => {
   var messageSplit = msg.content.split(' ');
   var command = messageSplit.shift();
 
-  if(command === "!submit"){
+  if(command.toLowerCase() === "!submit"){
 
     var dev = msg.member.roles.indexOf(config.dev);
     var hunter = msg.member.roles.indexOf(config.hunter);
@@ -36,137 +30,38 @@ bot.on('messageCreate', (msg) => {
     var user = msg.author.username + "#" + msg.author.discriminator;
 
     if(dev > -1 || hunter > -1 || admin > -1){
-      if(msg.channel.id === config.ios){
-        sendToRedis("i", messageSplit.join(' '), msg.channel.id, user, msg.author.id);
+      if(!msg.content.match(/\|/g)){
+        bot.createMessage(msg.channel.id, "<@" + msg.author.id + "> Please use the proper format. `!submit <title> | <report>`");
+      }else if(msg.content.match(/\|/g).length < 2){
+        var str = messageSplit.join(' ');
 
-      }else if(msg.channel.id === config.android){
-        sendToRedis("a", messageSplit.join(' '), msg.channel.id, user, msg.author.id);
+        const pipe = str.indexOf("|");
+        const header = str.substr(0, pipe).trim();
+        const report = str.substr(pipe+1).trim();
 
-      }else if(msg.channel.id === config.canary){
-        sendToRedis("c", messageSplit.join(' '), msg.channel.id, user, msg.author.id);
+        if(msg.channel.id === config.ios){
+          var listID = config.iosTrello;
+          sendToTrello(listID, header, user + "\n" + report, msg.channel.id);
 
-      }
-    }
-  }
+        }else if(msg.channel.id === config.android){
+          var listID = config.androidTrello;
+          sendToTrello(listID, header, user + "\n" + report, msg.channel.id);
 
-  if(command === "!header"){
+        }else if(msg.channel.id === config.canary){
+          var listID = config.canaryTrello;
+          sendToTrello(listID, header, user + "\n" + report, msg.channel.id);
 
-    var dev = msg.member.roles.indexOf(config.dev);
-    var hunter = msg.member.roles.indexOf(config.hunter);
-    var admin = msg.member.roles.indexOf(config.admin);
-
-    if(dev > -1 || hunter > -1 || admin > -1){
-      var getKey;
-      var header;
-      switch (msg.channel.id) {
-        case config.ios:
-          if(!iOSSKey){
-            getKey = messageSplit.shift();
-          }else if(!!iOSSKey && messageSplit[0] === iOSSKey){
-            getKey = messageSplit.shift();
-            header = messageSplit.join(' ');
-          }else{
-            getKey = iOSSKey;
-            header = messageSplit.join(' ');
-          }
-          break;
-        case config.android:
-          if(!androidSKey){
-            getKey = messageSplit.shift();
-          }else if(!!androidSKey && messageSplit[0] === androidSKey){
-            getKey = messageSplit.shift();
-            header = messageSplit.join(' ');
-          }else{
-            getKey = androidSKey;
-            header = messageSplit.join(' ');
-          }
-          break;
-        case config.canary:
-          if(!canarySKey){
-            getKey = messageSplit.shift();
-          }else if(!!canarySKey && messageSplit[0] === canarySKey){
-            getKey = messageSplit.shift();
-            header = messageSplit.join(' ');
-          }else{
-            getKey = canarySKey;
-            header = messageSplit.join(' ');
-          }
-          break;
-
-      }
-      client.hexists(getKey, "os", function(err, reply){
-        if(reply === 1){
-          header = messageSplit.join(' ');
-          setupTrello(getKey, header, msg.channel.id);
-        }else if(reply === 0){
-          bot.createMessage(msg.channel.id, "Report key is incorrect or missing.");
         }
-      });
+      }else{
+        bot.createMessage(msg.channel.id, "<@" + msg.author.id + "> Please use the proper format. `!submit <title> | <report>`");
+      }
     }
   }
 });
 
-function sendToRedis(system, bugReport, channelID, user, userID){
-  var key = system + Math.floor((Math.random() * 8999) + 1000);
-
-  client.hexists(key, "os", function(err, reply){
-    if(reply === 1){
-      sendToRedis(system, bugReport);
-    }else{
-      client.hset(key, "os", system);
-      client.hset(key, "report", user + "\n" + bugReport);
-
-      if(system === "c"){
-        if(!canarySKey){
-          canarySKey = key;
-        }else if(!!canarySKey){
-          canarySKey = undefined;
-        }
-      }else if(system === "a"){
-        if(!androidSKey){
-          androidSKey = key;
-        }else if(!!androidSKey){
-          androidSKey = undefined;
-        }
-      }else if(system === "i"){
-        if(!iOSSKey){
-          iOSSKey = key;
-        }else if(!!iOSSKey){
-          iOSSKey = undefined;
-        }
-      }
-      bot.createMessage(channelID, "<@" + userID +"> Type `!header <key> <name of bug>` to finish the bug report. Your key is: `" + key + "`");
-    }
-  });
-}
-
-function setupTrello(key, header, channelID){
-  client.hget(key, "os", function(err, os){
-    client.hget(key, "report", function(err, report){
-      if(!!err){
-        console.log(err);
-      }
-      if(os.toString() === "c"){
-        var listID = config.canaryTrello;
-        sendToTrello(listID, header, report.toString(), channelID, key);
-      }else if(os.toString() === "a"){
-        var listID = config.androidTrello;
-        sendToTrello(listID, header, report.toString(), channelID, key);
-      }else if(os.toString() === "i"){
-        var listID = config.iosTrello;
-        sendToTrello(listID, header, report.toString(), channelID, key);
-      }
-    });
-  });
-}
-
-function sendToTrello(listID, header, report, channelID, key){
+function sendToTrello(listID, header, report, channelID){
   var creationSuccess = function(data) {
-    bot.createMessage(channelID, "Report `" + key + "` added to Trello");
-    canarySKey = undefined;
-    iOSSKey = undefined;
-    androidSKey = undefined;
-    client.del(key);
+    bot.createMessage(channelID, "Report added to Trello");
   };
   var newCard = {
     name: header,

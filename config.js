842e146d37d13b32f74d02d6c19a200646b929cd
Logiz <l.samemami@gmail.com>
2016-11-04 18:40:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,18 +3,22 @@ var config = {
   trelloKey: '',
   trelloToken: '',
 
-  ios: '202491590390841344',               //ios text Channel
-  android: '232568032394870784',           //android text Channel
-  canary: '197038744908333066',            //canary text Channel
+  iosChannel: '202491590390841344',               //ios text Channel
+  androidChannel: '232568032394870784',           //android text Channel
+  canaryChannel: '197038744908333066',            //canary text Channel
+  modLogChannel: "241625778368806912",            //mod log text Channel
+  bugHunterChannel: '217764019661045761',         //bug hunter text Channel
 
-  dev: '197042389569765376',               //Dev role
-  hunter: '197042209038663680',            //Bug hunter Role
-  admin: '197042322939052032',             //Admin role
-  androidAlpha: '234838349800538112',      //Android Alpha role
-  iosTestflight: '234838392464998402',     //iOSTestFlight role
+  devRole: '197042389569765376',                  //dev role
+  hunterRole: '197042209038663680',               //bug hunter role
+  adminRole: '197042322939052032',                //admin role
+  androidAlphaRole: '234838349800538112',         //android alpha role
+  iosTestflightRole: '234838392464998402',        //ios testflight role
 
-  iosCard: '57fd4af58042f56c271d46b3',     //trello iosCard
-  androidCard: '57f2a30d861fa9b5816c9717', //trello androidCard
-  canaryCard: '5771677fda45a436c5b8d255'   //trello canaryCard
+  iosCard: '57fd4af58042f56c271d46b3',            //ios user report card
+  androidCard: '57f2a30d861fa9b5816c9717',        //android user report card
+  canaryCard: '5771677fda45a436c5b8d255',         //canary user report card
+
+  delayInMS: 15000                                //delay in ms for the bot to remove messages
 }
 module.exports = config;

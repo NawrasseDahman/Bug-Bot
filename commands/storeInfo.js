3a20403703ea1b6acf19bf6f3449ca28bcbe399d
Logiz <l.samemami@gmail.com>
2017-05-15 20:31:27 +0200

    Update storeInfo.js

--- commands/storeInfo.js
+++ commands/storeInfo.js
@@ -77,12 +77,12 @@ let storeSysInfo = {
         //Edit existing database entry
         db.run("UPDATE users SET " + whichOS + " = ? WHERE userid = ?", [systemInfo, userID]);
         utils.botReply(bot, userID, channelID, "your new " + utils.toTitleCase(whichOS) + " settings have been saved", command, msg.id);
-        bot.createMessage(config.channel.modLogChannel, ":floppy_disk: **" + userTag + "** changed **" + utils.toTitleCase(whichOS) + "** -> `" + systemInfo + "`"); //user changed their --- info
+        bot.createMessage(config.channels.modLogChannel, ":floppy_disk: **" + userTag + "** changed **" + utils.toTitleCase(whichOS) + "** -> `" + systemInfo + "`"); //user changed their --- info
       }else if(!dbRowReply){
         //Create new database entry
         db.run("INSERT INTO users (userid, " + whichOS + ") VALUES(?, ?)", [userID, systemInfo]);
         utils.botReply(bot, userID, channelID, "your new " + utils.toTitleCase(whichOS) + " settings have been saved", command, msg.id);
-        bot.createMessage(config.channel.modLogChannel, ":floppy_disk: **" + userTag + "** added **" + utils.toTitleCase(whichOS) + "** -> `" + systemInfo + "`"); //user added their --- info
+        bot.createMessage(config.channels.modLogChannel, ":floppy_disk: **" + userTag + "** added **" + utils.toTitleCase(whichOS) + "** -> `" + systemInfo + "`"); //user added their --- info
       }
     });
   },

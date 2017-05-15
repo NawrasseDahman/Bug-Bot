4f6c14ae0ab8ba1108b9d79429492ac82a5d0825
Logiz <l.samemami@gmail.com>
2017-05-15 18:28:06 +0200

    Update configEdit.js

--- configEdit.js
+++ configEdit.js
@@ -3,15 +3,15 @@ var customConfig = {
   trelloKey: '',
   trelloToken: '',
 
-  delayInS: 6, // in seconds
-  minuteDelay: 60, // 60 seconds
-  maxDelay: 120, // submit message delete timer
-  backupTimer: 6, // in hours (for example 24 hours);
+  delayInS: 6,          // in seconds
+  minuteDelay: 60,      // 60 seconds
+  maxDelay: 120,        // submit message delete timer
+  backupTimer: 6,       // in hours (for example 24 hours);
 
-  approveAttempts: 3, // number of approvals before the bug is approved
-  denyAttempts: 3, // number of denials before the bug is denied
+  approveAttempts: 3,   // number of approvals before the bug is approved
+  denyAttempts: 3,      // number of denials before the bug is denied
 
-  reproAttempts: 5 // number of repro attempts before they no longer attach to the report in chat
+  reproAttempts: 5      // number of repro attempts before they no longer attach to the report in chat
 }
 
 module.exports = customConfig;
